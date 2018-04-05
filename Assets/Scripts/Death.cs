b0d5b2860091fad280bb15650343754ce834c032
Rosalie Fragnito <rf02095@georgiasouthern.edu>
2018-04-05 19:57:53 -0400

    GameUpdates

---
+++ Assets/Scripts/Death.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Death : MonoBehaviour {
+
+	public Transform target;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter(Collidor other) {
+
+		if (other.gameObject.CompareTag("Death")){
+			this.transform.position = target.position;
+	}
+}
